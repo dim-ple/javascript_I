8142bd0ef597478d5703873f3237f7371e93f1ac
Harrison Goehring <98855700+dim-ple@users.noreply.github.com>
2022-12-05 15:01:47 -0500

    Add files via upload

---
+++ project01-02-hgoehring/project01-02-hgoehring.js
@@ -0,0 +1,15 @@
+/*  Hands-On Project 1-2
+    Author: Harrison Goehring
+    Date:  9/6/2022
+*/
+
+//define variables for service name and service speed
+let service1Name = "Express";
+let service2Name = "Super";
+let service3Name = "Extreme";
+let service4Name = "Ultra";
+
+let service1Speed = "100 Mbps";
+let service2Speed = "250 Mbps";
+let service3Speed = "1 Gig";
+let service4Speed = "2 Gig";
