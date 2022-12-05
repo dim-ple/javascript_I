944a20bfbc169e19757bc77019464bb07fad3b9f
Harrison Goehring <98855700+dim-ple@users.noreply.github.com>
2022-12-05 15:12:53 -0500

    Add files via upload

---
+++ project02-03-hgoehring/project02-03-hgoehring.js
@@ -0,0 +1,43 @@
+/*    Project 02-03
+      Author: Harrison Goehring
+      Date: 9/26/2022
+ */
+
+document.getElementById("square").onmouseover = function() {
+      
+      document.getElementById("message").innerHTML = "You're hovering over the square";
+
+}
+
+
+document.getElementById("square").onmouseout = function() {
+
+      document.getElementById("message").innerHTML = "";
+
+} 
+
+document.getElementById("triangle").onmouseover = function() {
+      
+      document.getElementById("message").innerHTML = "You're hovering over the triangle";
+
+}
+
+
+document.getElementById("triangle").onmouseout = function() {
+
+      document.getElementById("message").innerHTML = "";
+
+} 
+
+document.getElementById("circle").onmouseover = function() {
+      
+      document.getElementById("message").innerHTML = "You're hovering over the circle";
+
+}
+
+
+document.getElementById("circle").onmouseout = function() {
+
+      document.getElementById("message").innerHTML = "";
+
+} 
